eee720b5ce586f86d89ed169df2b959d8c9b8b1d
khaledHassan2 <khaledamalb22@gmail.com>
2024-12-31 22:23:06 +0200

    Q1)Define 3D Point Class and the basic Constructors (use chaining in constructors).

---
+++ Assignment/NewFolder/Point3D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.NewFolder
+{
+    internal class Point3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        
+        public Point3D() : this(0, 0, 0) { }
+
+        public Point3D(double x, double y) : this(x, y, 0) { }
+
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"Point({X}, {Y}, {Z})";
+        }
+
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using Assignment.NewFolder;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1)Define 3D Point Class and the basic Constructors (use chaining in constructors).
+
+            //Point3D point = new Point3D();
+            //Console.WriteLine($"p3{point}");
+
+            //Point3D point2 = new Point3D(1, 2);
+            //Console.WriteLine($"p3{point2}");
+
+            //Point3D point3 = new Point3D(1, 2, 3);
+            //Console.WriteLine($"p3{point3}"); 
+            #endregion
+        }
+    }
+}
